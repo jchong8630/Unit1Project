f93e905022af64529f1778f5765b4dc4b74aeb18
BT_4N2_23 <jchong8630@bths.edu>
2022-09-21 12:15:58 -0400

    Fixing rounding errors

--- src/Project.java
+++ src/Project.java
@@ -10,18 +10,22 @@ public class Project {
         System.out.print("Number Of People: ");
         int numPeople = s.nextInt();
         double tipCost = totalBill * tipPercent;
+        tipCost = Math.round((tipCost + .001) * 100.0) / 100.0  ;
+        System.out.println(tipCost);
         double tipPerPerson = tipCost / numPeople;
         double costPerPerson = totalBill / numPeople;
+        costPerPerson = Math.round((costPerPerson + .001) * 100.0) / 100.0;
         double totalPerPerson = costPerPerson + tipPerPerson;
         totalPerPerson = Math.round(totalPerPerson * 100.0) / 100.0;
-        if ((totalPerPerson * numPeople) != totalBill) {
-            totalPerPerson += .01;
+        if ((totalPerPerson * numPeople) != (totalBill + tipCost)) {
+            totalPerPerson += .011;
+            totalPerPerson = Math.round(totalPerPerson * 100.0) / 100.0;
         }
 
         System.out.println(tipPerPerson);
         System.out.println(totalPerPerson);
 
-
+//https://github.com/jchong8630/Unit1Project
 
     }
 }
