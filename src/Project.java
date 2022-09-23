1d200eb48cc6c2ddffc53c00d73de24ddfc3deab
Justin Chong <jchong8630@bths.edu>
2022-09-22 21:04:16 -0400

    Finished display and added comments

--- src/Project.java
+++ src/Project.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 public class Project {
     public static void main(String[] args) {
         Scanner s = new Scanner(System.in);
+
+        //Input Variables
         System.out.print("Total Bill: ");
         double totalBill = s.nextDouble();
         System.out.print("Tip Percent: ");
@@ -9,24 +11,29 @@ public class Project {
         tipPercent *= .01;
         System.out.print("Number Of People: ");
         int numPeople = s.nextInt();
+
+        //Non Input Variables
         double tipCost = totalBill * tipPercent;
         tipCost = Math.round((tipCost + .001) * 100.0) / 100.0  ;
         double tipPerPerson = tipCost / numPeople;
-        System.out.println(tipPerPerson);
         tipPerPerson = Math.round((tipPerPerson + .001) * 100.0) / 100.0;
-        System.out.println(tipPerPerson);
         double costPerPerson = totalBill / numPeople;
         double totalPerPerson = costPerPerson + tipPerPerson;
         totalPerPerson = Math.round(totalPerPerson * 100.0) / 100.0;
-        if ((totalPerPerson * numPeople) < (totalBill + tipCost)) {
+        double completeBill = totalBill + tipCost;
+
+        //Increase totalPerPerson by 1 cent if the amount calculated is less than the completeBill
+        if ((totalPerPerson * numPeople) < (completeBill)) {
             totalPerPerson += .01;
             totalPerPerson = Math.round(totalPerPerson * 100.0) / 100.0;
         }
 
+        //Display
+        System.out.println();
+        System.out.println("Total Tip Cost: $" + tipCost);
+        System.out.println("Total Cost: $" + completeBill);
         System.out.println("Tip Per Person: $" + tipPerPerson);
         System.out.println("Total Per Person: $" + totalPerPerson);
-
-//https://github.com/jchong8630/Unit1Project
 
     }
 }
