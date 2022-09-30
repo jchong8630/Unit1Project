2f1f8ee931009c4fc4511ad0878098a808249306
BT_4N2_23 <jchong8630@bths.edu>
2022-09-30 11:46:28 -0400

    Updated to use classes and runner

---
+++ src/Runner.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Runner {
+    public static void main(String[] args) {
+        TipCalculator customer = new TipCalculator();
+        customer.calculator();
+
+    }
+}
